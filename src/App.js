e762fecfb3c2f5d1b9fc11c7924adee7cbf54263
Mark Pieciak <mpieciak18@msn.com>
2022-01-03 16:28:28 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Inbox from "./pages/Inbox"
+import Post from "./pages/Post"
+import Profile from "./pages/Profile"
+import Settings from "./pages/Settings"
 
 const App = () => {
     return (
         <BrowserRouter basename={process.env.PUBLIC_URL}>
-
+            <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/inbox' element={<Inbox />} />
+                <Route exact path='/post' element={<Post />} />
+                <Route exact path='/profile' element={<Profile />} />
+                <Route exact path='/settings' element={<Settings />} />
+            </Routes>
         </BrowserRouter>
     )
 }
