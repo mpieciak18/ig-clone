b751f23fff2f9be2f479c905235c1250a14939d4
Mark Pieciak <mpieciak18@msn.com>
2022-01-04 14:10:18 -0500

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Firebase modules
+import { initializeApp } from "firebase/app"
+
+// Firebase configuration settings
+const firebaseConfig = {
+    apiKey: "AIzaSyAJdE_RLKn6Y9YImHw5ZiOF-rdvQt5GIZk",
+    authDomain: "ig-clone-5b7ab.firebaseapp.com",
+    projectId: "ig-clone-5b7ab",
+    storageBucket: "ig-clone-5b7ab.appspot.com",
+    messagingSenderId: "183201976316",
+    appId: "1:183201976316:web:29b1157645349379792c79"
+}
+
+// Firebase app initialization
+const app = initializeApp(firebaseConfig)
