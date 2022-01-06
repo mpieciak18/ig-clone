f6ae7aef1d5d92381e5c6ccc3400c226e226663a
Mark Pieciak <mpieciak18@msn.com>
2022-01-06 16:41:22 -0500

    add like

---
+++ src/firebase/likes.js
@@ -0,0 +1,31 @@
+import { auth, db } from './firebase.js'
+import { findUser } from './users.js'
+import { 
+    collection,
+    doc,
+    setDoc
+ } from 'firebase/firestore'
+
+// Add like to users -> user -> posts -> post -> likes
+// and return the like id
+const addLike = async (postId, postOwnerId) => {
+    // First, set up data
+    const userId = auth.currentUser
+    const data = {
+        post: postId,
+        postOwner: postOwnerId,
+        user: userId
+    }
+    // Second, grab document reference
+    const usersRef = collection(db, 'users')
+    const userRef = doc(usersRef, postOwnerId)
+    const postsRef = collection(userRef, 'posts')
+    const postRef = doc(postsRef, postId)
+    const likesRef = collection(postRef, 'likes')
+    const likeRef = doc(likesRef)
+    // Third, add like to likes collection & return like id
+    await setDoc(likeRef, data)
+    return data
+}
+
+export default { addLike }
