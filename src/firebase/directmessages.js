df14a9931b12abe2bb74b46cca3d89e00134de89
Mark Pieciak <mpieciak18@msn.com>
2022-01-07 14:18:27 -0500

    update message db hierarchy

--- src/firebase/directmessages.js
+++ src/firebase/directmessages.js
@@ -1,14 +1,16 @@
 import { db, auth } from './firebase.js'
 import { doc, collection, setDoc } from 'firebase/firestore'
 
-// Send message, add to both users messages subcollections,
+// Send message, add to both users (user -> conversation -> message),
 // and return message id
 const sendMessage = async (message, date, recipientId) => {
     // First, add message to sender's subcollection
     const senderId = auth.currentUser.uid
     const usersRef = collection(db, 'users')
     const senderRef = doc(usersRef, senderId)
-    const senderMessagesRef = collection(senderRef, 'messages')
+    const senderConvosRef = collection(senderRef, 'conversations')
+    const senderConvoRef = doc(senderConvosRef, recipientId)
+    const senderMessagesRef = collection(senderConvoRef, 'messages')
     const senderMessageRef = doc(senderMessagesRef)
     const messageData = {
         sender: senderId,
@@ -19,10 +21,12 @@ const sendMessage = async (message, date, recipientId) => {
     await setDoc(senderMessageRef, messageData)
     // Second, add message to recipient's subcollection
     const messageId = senderMessageRef.id
-    const recipientRef = doc(usersRef, recipientId)
-    const recipientMessagesRef = collection(recipientRef, 'messages')
-    const recipientMessageRef = doc(recipientMessagesRef, messageId)
-    await setDoc(recipientMessageRef, messageData)
+    const recipRef = doc(usersRef, recipientId)
+    const recipConvosRef = collection(recipRef, 'conversations')
+    const recipConvoRef = doc(recipConvosRef, senderId)
+    const recipMessagesRef = collection(recipConvoRef, 'messages')
+    const recipMessageRef = doc(recipMessagesRef, messageId)
+    await setDoc(recipMessageRef, messageData)
     // Third, return message id
     return messageId
 }
