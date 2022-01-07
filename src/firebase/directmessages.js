35d95c768e97676678652c8fc84bad2676704e3a
Mark Pieciak <mpieciak18@msn.com>
2022-01-07 15:58:25 -0500

    use helper functions for db queries

--- src/firebase/directmessages.js
+++ src/firebase/directmessages.js
@@ -3,20 +3,43 @@ import {
     doc,
     collection,
     setDoc,
+    getDoc,
     getDocs,
+    query,
+    limit
 } from 'firebase/firestore'
+
+// Helper functions that return doc/collection references from db
+const getUsersRef = () => {
+    return collection(db, 'users')
+}
+const getUserRef = (userId) => {
+    return doc(getUsersRef(), userId)
+}
+const getConvosRef = (userId) => {
+    return collection(getUserRef(userId), 'conversations')
+}
+const getConvoRef = (userId, otherUserId) => {
+    return doc(getConvosRef(userId), otherUserId)
+}
+const getMessagesRef = (userId, otherUserId) => {
+    return collection(getConvoRef(userId, otherUserId), 'messages')
+}
+const getMessageRef = (userId, otherUserId, messageId=null) => {
+    if (messageId == null) {
+        return doc(getMessagesRef(userId, otherUserId))
+    } else {
+        return doc(getMessagesRef(userId, otherUserId), messageId)
+    }
+}
 
 // Send message, add to both users (user -> conversation -> message),
 // and return message id
+// NOTE: the conversation id is the OTHER user's id (and vice versa in their convos)
 const sendMessage = async (message, date, recipientId) => {
     // First, add message to sender's subcollection
     const senderId = auth.currentUser.uid
-    const usersRef = collection(db, 'users')
-    const senderRef = doc(usersRef, senderId)
-    const senderConvosRef = collection(senderRef, 'conversations')
-    const senderConvoRef = doc(senderConvosRef, recipientId)
-    const senderMessagesRef = collection(senderConvoRef, 'messages')
-    const senderMessageRef = doc(senderMessagesRef)
+    const senderMessageRef = getMessageRef(senderId, recipientId)
     const messageData = {
         sender: senderId,
         recipient: recipientId,
@@ -26,11 +49,7 @@ const sendMessage = async (message, date, recipientId) => {
     await setDoc(senderMessageRef, messageData)
     // Second, add message to recipient's subcollection
     const messageId = senderMessageRef.id
-    const recipRef = doc(usersRef, recipientId)
-    const recipConvosRef = collection(recipRef, 'conversations')
-    const recipConvoRef = doc(recipConvosRef, senderId)
-    const recipMessagesRef = collection(recipConvoRef, 'messages')
-    const recipMessageRef = doc(recipMessagesRef, messageId)
+    const recipMessageRef = getMessageRef(recipientId, userId, messageId)
     await setDoc(recipMessageRef, messageData)
     // Third, return message id
     return messageId
@@ -39,11 +58,7 @@ const sendMessage = async (message, date, recipientId) => {
 // Retrieve single conversation & return array of message objects
 const retrieveSingleConvo = async (otherUserId) => {
     const userId = auth.currentUser.uid
-    const usersRef = collection(db, 'users')
-    const userRef = doc(usersRef, userId)
-    const convosRef = collection(userRef, 'conversations')
-    const convoRef = doc(convosRef, otherUserId)
-    const messagesRef = collection(convoRef, 'messages')
+    const messagesRef = getMessagesRef(userId, otherUserId)
     const messageDocs = await getDocs(messagesRef)
     let messages = []
     messageDocs.forEach((doc) => {
@@ -56,4 +71,19 @@ const retrieveSingleConvo = async (otherUserId) => {
     return messages
 }
 
-export default { sendMessage }
+// Retrieve latest message from a conversation
+// NOTE: the other user's ID is the conversation ID for the user
+// and the user's ID is the conversation ID for the other user
+const retrieveLatestMessage = async (otherUserId) => {
+    const userId = auth.currentUser.uid
+    const convoRef = getConvoRef(userId, otherUserId)
+    const messageRef = query(convoRef, limit(1))
+    const messageDoc = await getDoc(messageRef)
+    const message = {
+        id: messageDoc.id,
+        data: messageDoc.data()
+    }
+    return message
+}
+
+export default { sendMessage, retrieveLatestMessage }
