abb248621d80c13f4e0d07dd2ad6a2707b1345d6
Mark Pieciak <mpieciak18@msn.com>
2022-01-09 17:42:22 -0500

    add load button & useEffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import '../styles/Home.css'
 import { findPosts } from '../firebase/posts.js'
 import { Navbar } from '../components/Navbar.js'
 import { Post } from '../components/Post.js'
+import { useEffect } from 'react'
 
 const Home = async () => {
     const { user } = props
@@ -12,6 +13,23 @@ const Home = async () => {
     // Init posts state
     const postsArr = await findPosts(postsNumber)
     const [posts, setPosts] = useState(postsArr)
+
+    // Load-more function that updates the posts reel
+    const loadMore = () => {
+        const newPostsNumber = postsNumber + 10
+        setPostsNumber(newPostsNumber)
+    }
+
+    // Load More button
+    const loadMoreButton = (
+        <div id='load-more-button' onClick={loadMore}>Load More</div>
+    )
+
+    // Update posts state when postsNumber state changes
+    useEffect(async () => {
+        const newPostsArr = await findPosts(postsNumber)
+        setPosts(newPostsArr)
+    }, postsNumber)
 
     const Posts = (
         <div id='home-posts'>
