ec797f3959ef537b84322a088fe337f267eb030c
Mark Pieciak <mpieciak18@msn.com>
2022-01-10 16:58:47 -0500

    add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import '../styles/Navbar.css'
+
+const Navbar = (props) => {
+    const [user] = props
+
+    return (
+        <div id="navbar">
+            {/* Visible only on mobile screen dimensions*/}
+            <div id="header-mobile">
+                <img class="logo" />
+                <img class="messages-button" />
+            </div>
+            <div id="footer-mobile">
+                <img class="home-button" />
+                <img class="search-button" />
+                <img class="add-post-button" />
+                <img class="notifications-button" />
+                <img class="messages-button" />
+            </div>
+            {/* Visible only on desktop screen dimensions*/}
+            <div id="header-desktop">
+                <div id="header-desktop-left">
+                    <img class="logo"></img>
+                </div>
+                <div id="header-desktop-right">
+                    <img class="home-button" />
+                    <img class="search-button" />
+                    <img class="messages-button" />
+                    <img class="add-post-button" />
+                    <img class="notifications-button" />
+                    <img class="messages-button" />
+                </div>
+            </div>
+        </div>
+    )
+}
