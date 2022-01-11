a46d4e94a6679a815460babdd113460f9418a636
Mark Pieciak <mpieciak18@msn.com>
2022-01-11 10:17:33 -0500

    add post page

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,5 +1,41 @@
 import '../styles/Post.css'
+import { useParams } from 'react-router-dom'
+import { SinglePost } from '../components/SinglePost.js'
+import { findSinglePost } from '../firebase/posts.js'
+import { Navbar } from '../components/Navbar.js'
 
 const Post = (props) => {
-    
+    const [user] = props
+
+    // Extract post id from url parameters
+    const { postOwnerId, postId} = useParams()
+
+    // Get post data from database
+    const post = findSinglePost(postId, postOwnerId)
+
+    const BackButton = (
+        <div id='post-back-button'>
+            <div id='back-arrow'>⇽</div>
+            <div id='back-text'>Back to Profile</div>
+        </div>
+    )
+
+    return (
+        <div id="post" class="page">
+            <Navbar user={user} />
+            {BackButton}
+            <SinglePost 
+                page="true" 
+                id={postId}
+                text={post.data.text}
+                image={post.data.image}
+                date={post.data.date}
+                postOwnerId={postOwnerId}
+                likes={post.data.likes}
+                user={user}
+            />
+        </div>
+    )
 }
+
+export { Post }
