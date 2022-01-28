3eb8d82ebbfbd353a642aad8a2930dc3091270e6
Mark Pieciak <mpieciak18@msn.com>
2022-01-28 09:43:47 -0500

    add error message

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -49,13 +49,29 @@ const Settings = (props) => {
             image = file.name
             await uploadFile('images', file)
         }
-        await updateUser(
+        const possibleError = await updateUser(
             image,
             e.target.name.value,
             e.target.username.value,
             e.target.bio.value
         )
+        if (possibleError == null) {
+            // Redirect to own profile upon successful settings update
+            return <Navigate to={`/${user.id}`} />
+        } else {
+            setErrorClass('visible')
+            setTimeout(() => {setErrorClass('hidden')}, 2000)
+        }
     }
+    
+    // Display error upon unsuccessful settings update
+    const [errorClass, setErrorClass] = useState('hidden')
+
+    const errorMessage = (
+        <div id='settings-error' className={errorClass}>
+            There was an error! Please try again.
+        </div>
+    )
 
     // Display pop-up upon new user registration
     const [welcomeOn, setWelcomeOn] = useState(false)
@@ -82,33 +98,36 @@ const Settings = (props) => {
     return (
         <div id='settings' className='page'>
             {welcomeMessage}
-            <div id='settings-title'>Settings</div>
-            <img id='settings-image'/>
-            <form id='settings-form' onSubmit={updateSettings}>
-                <input 
-                    ref={imageRef} 
-                    type="file" 
-                    id="settings-image" 
-                    name="image" 
-                    accept=".jpg, .jpeg, .png" 
-                    onChange={validateImage}
-                >
-                    <img src={filePreview} />
-                </input>
-                <div id='settings-image-footer'>File size limit: 5 mb</div>
-                <label id='settings-name-label' for='name'>Your Name:</label>
-                <input id='settings-name-input' name='name' type='text' value={user.name}></input>
-                <label id='settings-username-label' for='username'>Your Display Name:</label>
-                <input id='settings-username-input' name='username' type='text' value={user.username}></input>
-                <label id='settings-bio-label' for='bio'>Your Bio:</label>
-                <input id='settings-bio-input' name='bio' type='text' value={user.bio}></input>
-                <div id='settings-buttons'>
-                    <Link to={`/profile/${user.id}`}>
-                        <button type='button'>Back to Profile</button>
-                    </Link>
-                    <button type={formButton}>Update Settings</button>
-                </div>
-            </form>
+            <div id='settings-parent'>
+                <form id='settings-form' onSubmit={updateSettings}>
+                    {errorMessage}
+                    <div id='settings-header'>
+                        <div id='settings-title'>Settings</div>
+                        <img id='settings-image'/>
+                    </div>
+                    <input 
+                        ref={imageRef} 
+                        type="file" 
+                        id="settings-image" 
+                        name="image" 
+                        accept=".jpg, .jpeg, .png" 
+                        onChange={validateImage}
+                    >
+                        <img src={filePreview} />
+                    </input>
+                    <div id='settings-image-footer'>File size limit: 5 mb</div>
+                    <label id='settings-name-label' for='name'>Your Name:</label>
+                    <input id='settings-name-input' name='name' type='text' value={user.name}></input>
+                    <label id='settings-bio-label' for='bio'>Your Bio:</label>
+                    <input id='settings-bio-input' name='bio' type='text' value={user.bio}></input>
+                    <div id='settings-buttons'>
+                        <Link to={`/profile/${user.id}`}>
+                            <button type='button'>Back to Profile</button>
+                        </Link>
+                        <button type={formButton}>Update Settings</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
