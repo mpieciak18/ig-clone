d2879a739afb02ed3056734a2f2589919b3e227d
Mark Pieciak <mpieciak18@msn.com>
2023-07-26 16:38:07 -0400

    begin creating schema & assign ports

---
+++ server/src/config/index.ts
@@ -0,0 +1,27 @@
+import { prodPort, localPort, testingPort } from './ports';
+import merge from 'lodash.merge';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+const stage = process.env.STAGE || 'local';
+
+let envConfig;
+if (stage === 'production') {
+	envConfig = prodPort;
+} else if (stage === 'testing') {
+	envConfig = localPort;
+} else {
+	envConfig = testingPort;
+}
+
+export const config = merge(
+	{
+		stage,
+		env: process.env.NODE_ENV,
+		port: 3001,
+		secrets: {
+			jwt: process.env.JWT_SECRET,
+			dbUrl: process.env.DATABASE_URL,
+		},
+	},
+	envConfig
+);
---
+++ server/src/config/ports.ts
@@ -0,0 +1,11 @@
+export const prodPort = {
+	port: process.env.PORT,
+};
+
+export const testingPort = {
+	port: 6000,
+};
+
+export const localPort = {
+	port: 3001,
+};
