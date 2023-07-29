c1c7d9e4f96fe0a6f2e43dbdf95ff8e918f1e8c2
Mark Pieciak <mpieciak18@msn.com>
2023-07-29 09:13:03 -0400

    create createNewUser handler for users

--- server/src/handlers/user.ts
+++ server/src/handlers/user.ts
@@ -1,3 +1,26 @@
-export const createNewUser = async (req, res, next) => {};
+import prisma from '../db';
+import { comparePasswords, createJwt, hashPassword } from '../modules/auth';
+
+// Creates a new user in the database and returns a signed JWT to the client.
+// Any error is assumed to be related to user input and is returned to the c.ient as such.
+export const createNewUser = async (req, res, next) => {
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: req.body.email,
+				username: req.body.username,
+				password: await hashPassword(req.body.password),
+				name: req.body.name,
+				bio: req.body.bio,
+				image: req.body.image,
+			},
+		});
+		const token = createJwt(user);
+		res.json({ token });
+	} catch (e) {
+		e.type = 'input';
+		next(e);
+	}
+};
 
 export const signIn = async (req, res, next) => {};
