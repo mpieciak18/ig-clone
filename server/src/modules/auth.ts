39470fcaccc8c4aa0d6d10e3268bb46c0e19041a
Mark Pieciak <mpieciak18@msn.com>
2023-07-29 08:27:50 -0400

    create protect function for auth

--- server/src/modules/auth.ts
+++ server/src/modules/auth.ts
@@ -12,7 +12,32 @@ export const createJwt = async (user: { id: any; username: any }) => {
 	return token;
 };
 
-export const protect = async () => {};
+export const protect = async (req, res, next) => {
+	const bearer = req.headers.authorization;
+	if (!bearer) {
+		res.status(401);
+		res.json({ message: 'Not Authorized' });
+		return;
+	}
+
+	const [, token] = bearer.split(' ');
+	if (!token) {
+		res.status(401);
+		res.json({ message: 'Invalid Token' });
+		return;
+	}
+
+	try {
+		const user = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = user;
+		next();
+	} catch (e) {
+		console.log(e);
+		res.status(401);
+		res.json({ message: 'Token Unverifiable' });
+		return;
+	}
+};
 
 export const comparePasswords = async () => {};
 
