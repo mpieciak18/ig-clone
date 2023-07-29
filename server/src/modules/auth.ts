5b5ae0143489f1f4853a48392369c3648b20beeb
Mark Pieciak <mpieciak18@msn.com>
2023-07-29 08:14:33 -0400

    update .gitignore

--- server/src/modules/auth.ts
+++ server/src/modules/auth.ts
@@ -1,4 +1,15 @@
-export const createJwt = async () => {};
+import jwt from 'jsonwebtoken';
+import * as bcrypt from 'bcrypt';
+
+export const createJwt = async (user: { id: any; username: any }) => {
+	const token = jwt.sign(
+		{
+			id: user.id,
+			username: user.username,
+		},
+		process.env.JWT_SECRET
+	);
+};
 
 export const protect = async () => {};
 
