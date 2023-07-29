22f3c436d7203853a07d8de2d3896d94f3b6ea71
Mark Pieciak <mpieciak18@msn.com>
2023-07-28 21:10:56 -0400

    add initial code to server file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,2 +1,19 @@
 import express from 'express';
 import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
+
+// initialize express
+const app = express();
+
+// add default middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+	res.status(200);
+	res.json({ message: 'hello' });
+});
+
+export default app;
