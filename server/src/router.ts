ed524b85083429c9f70aa4879dab77188874cf73
Mark Pieciak <mpieciak18@msn.com>
2023-07-28 21:04:55 -0400

    add router & server files

---
+++ server/src/router.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { body } from 'express-validator';
+
+const router = Router();
+
+// Synchronous error handler
+router.use((err, req, res, next) => {
+	//
+});
+
+export default router;
