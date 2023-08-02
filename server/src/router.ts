20b992003a796065ac0172139a1c711711ae4987
Mark Pieciak <58556319+mpieciak18@users.noreply.github.com>
2023-08-02 16:25:00 -0400

    create routes for follow model

--- server/src/router.ts
+++ server/src/router.ts
@@ -3,7 +3,18 @@ import { body } from 'express-validator';
 
 const router = Router();
 
-// add in routes for every API endpoint below
+// // // // // //
+//   Follow    //
+// // // // // //
+
+// Gets a user's given follows (e.g., to see who they follow)
+router.get('/follow/given');
+// Gets a user's received follows (e.g., to see their followers)
+router.get('/follow/received');
+// Creates a new follow when one user (the giver) follows another (the receiver)
+router.post('/follow');
+// Deletes a follow, based on follow id
+router.delete('/follow/:id');
 
 // synchronous error handler
 // // add code once all handlers + auth middleware are created // //
