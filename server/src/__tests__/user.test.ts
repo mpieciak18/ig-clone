2b11f5695c05f27ae1d11afb6c5c1d65b855c4f2
Mark Pieciak <58556319+mpieciak18@users.noreply.github.com>
2023-08-03 10:48:22 -0400

    add first test

---
+++ server/src/__tests__/user.test.ts
@@ -0,0 +1,17 @@
+import app from '../server';
+import supertest from 'supertest';
+
+describe('POST /create_new_user', () => {
+	it('should create a new user & return a web token + a 200 status', async () => {
+		const response = await supertest(app).post('/create_new_user').send({
+			email: 'test@test.com',
+			username: 'test22',
+			password: 'easy_as_abc_123',
+			name: 'Tester',
+			bio: "I'm a test account.",
+			image: 'https://images.rawpixel.com/image_png_1300/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTAxL3JtNjA5LXNvbGlkaWNvbi13LTAwMi1wLnBuZw.png',
+		});
+		expect(response.body.token).toBeDefined();
+		expect(response.status).toBe(200);
+	});
+});
