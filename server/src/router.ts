86ab1990cc043952d349acdc3cecdef5be812e43
Mark Pieciak <58556319+mpieciak18@users.noreply.github.com>
2023-08-03 10:07:18 -0400

    create routes for post model & update follow routes

--- server/src/router.ts
+++ server/src/router.ts
@@ -4,17 +4,32 @@ import { body } from 'express-validator';
 const router = Router();
 
 // // // // // //
-//   Follow    //
+//   Follows   //
 // // // // // //
 
 // Gets a user's given follows (e.g., to see who they follow)
-router.get('/follow/given');
+router.get('/follows/given');
 // Gets a user's received follows (e.g., to see their followers)
-router.get('/follow/received');
+router.get('/follows/received');
 // Creates a new follow when one user (the giver) follows another (the receiver)
-router.post('/follow');
-// Deletes a follow, based on follow id
-router.delete('/follow/:id');
+router.post('/follows');
+// Deletes a follow when one user (the giver) unfollows another (the receiver)
+router.delete('/follows');
+
+// // // // // //
+//    Posts    //
+// // // // // //
+
+// Gets (a limited number of) posts for home page
+router.get('/posts/all');
+// Gets (a limited number of) a user's posts
+router.get('/posts/user');
+// Gets a single post
+router.get('/posts/:id');
+// Updates a single post
+router.put('/posts');
+// Deletes a single posts
+router.delete('/posts');
 
 // synchronous error handler
 // // add code once all handlers + auth middleware are created // //
