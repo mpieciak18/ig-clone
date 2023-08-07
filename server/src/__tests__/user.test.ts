b451f2f806027627a0f56184973dbeacd312cf2d
Mark Pieciak <58556319+mpieciak18@users.noreply.github.com>
2023-08-07 11:16:24 -0400

    update tests to include sign-in and failed create-new-user

--- server/src/__tests__/user.test.ts
+++ server/src/__tests__/user.test.ts
@@ -14,4 +14,26 @@ describe('POST /create_new_user', () => {
 		expect(response.body.token).toBeDefined();
 		expect(response.status).toBe(200);
 	});
+	it('should fail to create a new user & return a 400 status', async () => {
+		const response = await supertest(app).post('/create_new_user').send({
+			email: 'test@test.com',
+			username: 'test22',
+			password: 'easy_as_abc_123',
+			name: 'Tester',
+			bio: "I'm a test account.",
+			image: 'https://images.rawpixel.com/image_png_1300/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTAxL3JtNjA5LXNvbGlkaWNvbi13LTAwMi1wLnBuZw.png',
+		});
+		expect(response.status).toBe(400);
+	});
+});
+
+describe('POST /sign_in', () => {
+	it('should login & return a web token + a 200 status', async () => {
+		const response = await supertest(app).post('/sign_in').send({
+			username: 'test22',
+			password: 'easy_as_abc_123',
+		});
+		expect(response.body.token).toBeDefined();
+		expect(response.status).toBe(200);
+	});
 });
