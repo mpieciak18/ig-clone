f2a3259c55fed4eec9b3ae021a34e6e9a95fed1f
Mark Pieciak <58556319+mpieciak18@users.noreply.github.com>
2023-10-17 15:01:51 -0400

    rewrite profile card to best practices

--- client/src/components/Profile/children/ProfileCard.js
+++ client/src/components/Profile/children/ProfileCard.js
@@ -19,12 +19,6 @@ const ProfileCard = (props) => {
 	// Init otherUser id/data state
 	const [otherUser, setOtherUser] = useState(null);
 
-	// Init profileCard component state
-	const [profileCard, setProfileCard] = useState(null);
-
-	// Init state for follows pop-up component
-	const [follows, setFollows] = useState(null);
-
 	// Init state to determine if pop-up shows following or followers
 	const [followingVsFollower, setFollowingVsFollower] = useState('following');
 
@@ -56,83 +50,60 @@ const ProfileCard = (props) => {
 		});
 	}, []);
 
-	useEffect(() => {
-		// if (otherUser != null && popUpState != null) {
-		if (otherUser != null) {
-			setProfileCard(
-				<div id='profile-card'>
-					<div id='profile-card-top'>
-						<img id='profile-card-icon' src={img} />
-						<div id='profile-card-text'>
-							<div id='profile-card-name'>
-								{otherUser.data.name}
-							</div>
-							<div id='profile-card-username'>
-								@{otherUser.data.username}
-							</div>
-						</div>
-					</div>
-					<div id='profile-card-bottom'>
-						<div id='profile-card-stats'>
-							<div id='profile-card-posts'>
-								<p className='profile-stats-child-num'>
-									{otherUser.data.posts}
-								</p>
-								<p className='profile-stats-child-type'>
-									Posts
-								</p>
-							</div>
-							<div
-								id='profile-card-following'
-								onClick={clickFollowing}
-							>
-								<p className='profile-stats-child-num'>
-									{otherUser.data.following}
-								</p>
-								<p className='profile-stats-child-type'>
-									Following
-								</p>
-							</div>
-							<div
-								id='profile-card-followers'
-								onClick={clickFollowers}
-							>
-								<p className='profile-stats-child-num'>
-									{otherUser.data.followers}
-								</p>
-								<p className='profile-stats-child-type'>
-									Followers
-								</p>
-							</div>
-						</div>
-						<div id='profile-card-bio'>{otherUser.data.bio}</div>
-					</div>
-					{follows}
-				</div>
-			);
-		}
-	}, [otherUser, follows]);
-
 	// Update follows state if followsOn state changes
 	useEffect(() => {
-		if (popUpState != null) {
-			const body = document.querySelector('body');
-			if (popUpState.followsOn == false) {
-				body.style.overflow = 'auto';
-				setFollows(null);
-			} else {
-				body.style.overflow = 'hidden';
-				setFollows(
-					<Follows
-						otherUserId={otherUserId}
-						initTab={followingVsFollower}
-					/>
-				);
-			}
+		const body = document.querySelector('body');
+		if (popUpState?.followsOn) {
+			body.style.overflow = 'hidden';
+		} else {
+			body.style.overflow = 'auto';
 		}
 	}, [popUpState]);
 
-	return profileCard;
+	// return profileCard;
+
+	return otherUser ? (
+		<div id='profile-card'>
+			<div id='profile-card-top'>
+				<img id='profile-card-icon' src={img} />
+				<div id='profile-card-text'>
+					<div id='profile-card-name'>{otherUser.data.name}</div>
+					<div id='profile-card-username'>
+						@{otherUser.data.username}
+					</div>
+				</div>
+			</div>
+			<div id='profile-card-bottom'>
+				<div id='profile-card-stats'>
+					<div id='profile-card-posts'>
+						<p className='profile-stats-child-num'>
+							{otherUser.data.posts}
+						</p>
+						<p className='profile-stats-child-type'>Posts</p>
+					</div>
+					<div id='profile-card-following' onClick={clickFollowing}>
+						<p className='profile-stats-child-num'>
+							{otherUser.data.following}
+						</p>
+						<p className='profile-stats-child-type'>Following</p>
+					</div>
+					<div id='profile-card-followers' onClick={clickFollowers}>
+						<p className='profile-stats-child-num'>
+							{otherUser.data.followers}
+						</p>
+						<p className='profile-stats-child-type'>Followers</p>
+					</div>
+				</div>
+				<div id='profile-card-bio'>{otherUser.data.bio}</div>
+			</div>
+			{popUpState?.followsOn ? (
+				<Follows
+					otherUserId={otherUserId}
+					initTab={followingVsFollower}
+				/>
+			) : null}
+		</div>
+	) : null;
 };
 
 export { ProfileCard };
