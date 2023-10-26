d4c0e7a6aa7e4fdf03671ade082cfd9ae03c1394
Mark Pieciak <58556319+mpieciak18@users.noreply.github.com>
2023-10-26 10:29:44 -0400

    add handlers for notifications

--- server/src/handlers/notification.ts
+++ server/src/handlers/notification.ts
@@ -74,69 +74,22 @@ export const deleteNotif = async (req, res, next) => {
 	res.json({ notification });
 };
 
-// Gets a follow based on the other user's ID (if it exists)
-export const findFollow = async (req, res, next) => {
-	// First, confirm other user exists
-	// If no user is found, handle it at the top-level (server.ts) as 500 error
-	let otherUser;
+// Gets the user's unread notifications
+export const getNotifsUnread = async (req, res, next) => {
+	// First, try to get unread notifications
+	// If nothing is return (as oppposed to an empty array),
+	// then handle it at the top-level (server.ts) as 500 error
+	let notifs;
 	try {
-		otherUser = await prisma.user.findUnique({
-			where: { id: req.body.id },
+		notifs = await prisma.notification.findMany({
+			where: { userId: req.user.id, read: false },
 		});
 	} catch (e) {
 		// DB errors are handled at top-level (server.ts) as 500 error
 		next(e);
 		return;
 	}
-	if (!otherUser) {
-		const e = new Error();
-		next(e);
-		return;
-	}
-
-	// Second, find if signed-in user followed other user
-	let givenFollow;
-	try {
-		givenFollow = await prisma.follow.findFirst({
-			where: { giverId: req.user.id, receiverId: req.body.id },
-		});
-	} catch (e) {
-		// DB errors are handled at top-level (server.ts) as 500 error
-		next(e);
-		return;
-	}
-
-	// Third, find if signed-in user is followed by other user
-	let receivedFollow;
-	try {
-		receivedFollow = await prisma.follow.findFirst({
-			where: { giverId: req.body.id, receiverId: req.user.id },
-		});
-	} catch (e) {
-		// DB errors are handled at top-level (server.ts) as 500 error
-		next(e);
-		return;
-	}
-
-	// Fourth, return data back to client
-	res.json({ givenFollow, receivedFollow });
-};
-
-// Gets the follows given by (any) user
-export const getGivenFollows = async (req, res, next) => {
-	// First, confirm if provided user exists
-	// If no user is found, handle it at the top-level (server.ts) as 500 error
-	let otherUser;
-	try {
-		otherUser = await prisma.user.findUnique({
-			where: { id: req.body.id },
-		});
-	} catch (e) {
-		// DB errors are handled at top-level (server.ts) as 500 error
-		next(e);
-		return;
-	}
-	if (!otherUser) {
+	if (!notifs) {
 		const e = new Error();
 		next(e);
 		return;
@@ -158,31 +111,32 @@ export const getGivenFollows = async (req, res, next) => {
 	res.json({ follows: givenFollows });
 };
 
-// Gets the follows received by (any) user
-export const getReceivedFollows = async (req, res, next) => {
-	// First, confirm if provided user exists
-	// If no user is found, handle it at the top-level (server.ts) as 500 error
-	let otherUser;
+// Gets the user's read notifications
+export const getNotifsRead = async (req, res, next) => {
+	// First, try to get unread notifications
+	// If nothing is return (as oppposed to an empty array),
+	// then handle it at the top-level (server.ts) as 500 error
+	let notifs;
 	try {
-		otherUser = await prisma.user.findUnique({
-			where: { id: req.body.id },
+		notifs = await prisma.notification.findMany({
+			where: { userId: req.user.id, read: true },
 		});
 	} catch (e) {
 		// DB errors are handled at top-level (server.ts) as 500 error
 		next(e);
 		return;
 	}
-	if (!otherUser) {
+	if (!notifs) {
 		const e = new Error();
 		next(e);
 		return;
 	}
 
-	// Second, retrieve received follows
-	let receivedFollows;
+	// Second, retrieve given follows
+	let givenFollows;
 	try {
-		receivedFollows = await prisma.follow.findMany({
-			where: { receiverId: req.body.id },
+		givenFollows = await prisma.follow.findMany({
+			where: { giverId: req.body.id },
 		});
 	} catch (e) {
 		// DB errors are handled at top-level (server.ts) as 500 error
@@ -191,5 +145,32 @@ export const getReceivedFollows = async (req, res, next) => {
 	}
 
 	// Third, return given follows back to client
-	res.json({ follows: receivedFollows });
+	res.json({ follows: givenFollows });
+};
+
+// Updates a notification (as read)
+export const updateNotifRead = async (req, res, next) => {
+	// Update notification
+	let notification;
+	try {
+		notification = await prisma.notification.update({
+			where: { id: req.body.id },
+			data: { read: true },
+		});
+	} catch (e) {
+		// If error, handle it as a 500 error
+		next(e);
+		return;
+	}
+
+	// While the previous try/catch (along with the 'protect' middleware) should catch all errors,
+	// this is added as an extra step of error handling (in case the update 'runs' but nothing is returned).
+	if (!notification) {
+		const e = new Error();
+		next(e);
+		return;
+	}
+
+	// Return updated user data
+	res.json({ notification });
 };
