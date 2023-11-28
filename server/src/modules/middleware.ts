304a043d154a4f1f143f5bc6a486f7f77699732e
Mark Pieciak <58556319+mpieciak18@users.noreply.github.com>
2023-11-28 11:43:53 -0500

    allow user update to work without file upload

--- server/src/modules/middleware.ts
+++ server/src/modules/middleware.ts
@@ -3,7 +3,9 @@ import { bucket } from '../config/gcloud';
 import { randomUUID } from 'crypto';
 
 export const handleInputErrors = (req, res, next) => {
+	console.log(req.body);
 	const errors = validationResult(req);
+	console.log(errors);
 	if (!errors.isEmpty()) {
 		res.status(400);
 		res.json({ errors: errors.array() });
@@ -14,7 +16,10 @@ export const handleInputErrors = (req, res, next) => {
 
 export const uploadImage = async (req, res, next) => {
 	try {
-		if (!req.file) {
+		if (!req.file && req.path == '/user') {
+			next();
+			return;
+		} else if (!req.file && req.path != '/user') {
 			res.status(400).send();
 			return;
 		}
