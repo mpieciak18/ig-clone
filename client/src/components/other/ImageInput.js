88704e2f961a8b9ec7e1076531800cbbf8010574
Mark Pieciak <58556319+mpieciak18@users.noreply.github.com>
2023-11-30 12:14:26 -0500

    update supported file formats

--- client/src/components/other/ImageInput.js
+++ client/src/components/other/ImageInput.js
@@ -1,81 +1,94 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react';
 
 const ImageInput = (props) => {
-    const { inputRef, setFile, setErrorClass } = props
+	const { inputRef, setFile, setErrorClass } = props;
 
-    const [filePreviewUrl, setFilePreviewUrl] = useState(null)
+	const [filePreviewUrl, setFilePreviewUrl] = useState(null);
 
-    const [filePreview, setFilePreview] = useState(null)
+	const [filePreview, setFilePreview] = useState(null);
 
-    const [imageInput, setImageInput] = useState(null)
+	const [imageInput, setImageInput] = useState(null);
 
-    const [overlayClass, setOverlayClass] = useState('inactive')
+	const [overlayClass, setOverlayClass] = useState('inactive');
 
-    const maxFileSize = 10 * 1024 * 1024 // 5 MB
+	const maxFileSize = 40 * 1024 * 1024; // 20 MB
 
-    // Returns true if passed file is an image
-    const isImage = (file) => {
-        const validTypes = ["image/png", "image/jpg", "image/jpeg"]
-        if (validTypes.includes(file.type)) {
-            return true
-        } else {
-            return false
-        }
-    }
+	// Returns true if passed file is an image
+	const isImage = (file) => {
+		const validTypes = [
+			'image/avif',
+			'image/png',
+			'image/jpeg',
+			'image/webp',
+		];
+		if (validTypes.includes(file.type)) {
+			return true;
+		} else {
+			return false;
+		}
+	};
 
-    // Runs when user selects image to upload
-    const validateImage = async () => {
-        if (inputRef.current.files[0].size > maxFileSize || 
-        isImage(inputRef.current.files[0]) == false) {
-            inputRef.current.value = ''
-            setFile(null)
-            setFilePreviewUrl(null)
-            setErrorClass('active')
-            setTimeout(() => {setErrorClass('inactive')}, 2000)
-        } else {
-            setFile(inputRef.current.files[0])
-            setFilePreviewUrl(URL.createObjectURL(inputRef.current.files[0]))
-        }
-    }
+	// Runs when user selects image to upload
+	const validateImage = async () => {
+		if (
+			inputRef.current.files[0].size > maxFileSize ||
+			isImage(inputRef.current.files[0]) == false
+		) {
+			inputRef.current.value = '';
+			setFile(null);
+			setFilePreviewUrl(null);
+			setErrorClass('active');
+			setTimeout(() => {
+				setErrorClass('inactive');
+			}, 2000);
+		} else {
+			setFile(inputRef.current.files[0]);
+			setFilePreviewUrl(URL.createObjectURL(inputRef.current.files[0]));
+		}
+	};
 
-    useEffect(() => {
-        setImageInput(
-            <input 
-                ref={inputRef} 
-                type="file" 
-                id="new-post-image-input" 
-                name="image" 
-                onChange={validateImage}
-                onMouseDown={() => setOverlayClass('active')}
-                onMouseUp={() => setOverlayClass('inactive')}
-                onMouseOver={() => setOverlayClass('active')}
-                onMouseOut={() => setOverlayClass('inactive')}
-            />
-        )
-    }, [inputRef])
+	useEffect(() => {
+		setImageInput(
+			<input
+				ref={inputRef}
+				type='file'
+				id='new-post-image-input'
+				name='image'
+				onChange={validateImage}
+				onMouseDown={() => setOverlayClass('active')}
+				onMouseUp={() => setOverlayClass('inactive')}
+				onMouseOver={() => setOverlayClass('active')}
+				onMouseOut={() => setOverlayClass('inactive')}
+			/>
+		);
+	}, [inputRef]);
 
-    useEffect(() => {
-        if (filePreviewUrl != null) {
-            setFilePreview(<img id='new-post-image-preview' src={filePreviewUrl} />)
-        } else {
-            setFilePreview(<div id='new-post-image-preview' />)
-        }
-    }, [filePreviewUrl])
+	useEffect(() => {
+		if (filePreviewUrl != null) {
+			setFilePreview(
+				<img id='new-post-image-preview' src={filePreviewUrl} />
+			);
+		} else {
+			setFilePreview(<div id='new-post-image-preview' />);
+		}
+	}, [filePreviewUrl]);
 
-    return (
-        <div
-            id='new-post-image-input-parent'
-            onPointerDown={() => setOverlayClass('active')}
-            onPointerUp={() => setOverlayClass('inactive')}
-            onMouseOver={() => setOverlayClass('active')}
-            onMouseOut={() => setOverlayClass('inactive')}
-        >
-            <label id='new-post-image-footer' htmlFor='image'>File size limit: 5 mb</label>
-            {imageInput}
-            {filePreview}
-            <div id='new-post-image-overlay' className={overlayClass} />
-        </div>
-    )
-}
+	return (
+		<div
+			id='new-post-image-input-parent'
+			onPointerDown={() => setOverlayClass('active')}
+			onPointerUp={() => setOverlayClass('inactive')}
+			onMouseOver={() => setOverlayClass('active')}
+			onMouseOut={() => setOverlayClass('inactive')}
+		>
+			<label id='new-post-image-footer' htmlFor='image'>
+				File size limit: 5 mb
+			</label>
+			{imageInput}
+			{filePreview}
+			<div id='new-post-image-overlay' className={overlayClass} />
+		</div>
+	);
+};
 
-export { ImageInput }
+export { ImageInput };
