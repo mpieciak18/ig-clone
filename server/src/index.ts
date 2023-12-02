b418dbcae5770b085b74d3e91e9eb0836d93f714
Mark Pieciak <58556319+mpieciak18@users.noreply.github.com>
2023-12-02 14:07:14 -0500

    add websocket for conversations

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,61 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import { config } from './config/index';
 import app from './server';
+// imports for websockets
+import { createServer } from 'http';
+import { Server as SocketIOServer } from 'socket.io';
+import {
+	handleInputErrors,
+	retrieveUserFromToken,
+	createMessage,
+} from './modules/websocket';
 
-app.listen(config.port, () => {
+// configure websockets
+const httpServer = createServer(app);
+const io = new SocketIOServer(httpServer);
+
+// validates an incoming message from a websocket
+io.use((socket, next) => {
+	const token = socket.handshake.auth.token;
+
+	// Verify token
+	try {
+		const user = retrieveUserFromToken(token);
+		// @ts-ignore
+		socket.user = user;
+		next();
+	} catch (e) {
+		next(e);
+	}
+});
+
+//
+io.on('connection', (socket) => {
+	console.log('a user connected');
+
+	socket.on('sendMessage', (message) => {
+		const errors = handleInputErrors(message);
+		if (errors) {
+			socket.emit('inputError', errors);
+			return;
+		}
+
+		// @ts-ignore
+		await createMessage(data, socket, socket.decoded);
+
+		io.emit('newMessage', message);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
+});
+
+httpServer.listen(config.port, () => {
 	console.log(`hello on http://localhost:${config.port}`);
 });
 
+// error handling
 process.on('uncaughtException', () => {
 	console.log('uncaught exception (sync) at top level of node process');
 });
