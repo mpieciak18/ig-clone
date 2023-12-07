3f91d920e178824893fdd145640c18188641d361
Mark Pieciak <58556319+mpieciak18@users.noreply.github.com>
2023-12-07 00:19:15 -0500

    add types to follows

--- server/src/handlers/follow.ts
+++ server/src/handlers/follow.ts
@@ -1,9 +1,16 @@
+import { NextFunction, Response } from 'express';
 import prisma from '../db';
+import { AuthReq, HasId, HasLimit } from '../types/types';
+import { Follow, User } from '@prisma/client';
 
 // Creates a new follow
-export const createFollow = async (req, res, next) => {
+export const createFollow = async (
+	req: AuthReq & HasId,
+	res: Response,
+	next: NextFunction
+) => {
 	// First, confirm other user exists
-	let otherUser;
+	let otherUser: User | undefined;
 	try {
 		otherUser = await prisma.user.findUnique({
 			where: { id: req.body.id },
@@ -22,7 +29,7 @@ export const createFollow = async (req, res, next) => {
 	}
 
 	// Second, create follow
-	let follow;
+	let follow: Follow | undefined;
 	try {
 		follow = await prisma.follow.create({
 			data: { giverId: req.user.id, receiverId: req.body.id },
@@ -45,9 +52,13 @@ export const createFollow = async (req, res, next) => {
 };
 
 // Deletes a follow
-export const deleteFollow = async (req, res, next) => {
+export const deleteFollow = async (
+	req: AuthReq & HasId,
+	res: Response,
+	next: NextFunction
+) => {
 	// First, delete follow
-	let follow;
+	let follow: Follow | undefined;
 	try {
 		follow = await prisma.follow.delete({
 			where: { id: req.body.id },
@@ -70,10 +81,14 @@ export const deleteFollow = async (req, res, next) => {
 };
 
 // Gets a follow based on the other user's ID (if it exists)
-export const findFollow = async (req, res, next) => {
+export const findFollow = async (
+	req: AuthReq & HasId,
+	res: Response,
+	next: NextFunction
+) => {
 	// First, confirm other user exists
 	// If no user is found, handle it at the top-level (server.ts) as 500 error
-	let otherUser;
+	let otherUser: User | undefined;
 	try {
 		otherUser = await prisma.user.findUnique({
 			where: { id: req.body.id },
@@ -90,7 +105,7 @@ export const findFollow = async (req, res, next) => {
 	}
 
 	// Second, find if signed-in user followed other user
-	let givenFollow;
+	let givenFollow: Follow | undefined;
 	try {
 		givenFollow = await prisma.follow.findFirst({
 			where: { giverId: req.user.id, receiverId: req.body.id },
@@ -102,7 +117,7 @@ export const findFollow = async (req, res, next) => {
 	}
 
 	// Third, find if signed-in user is followed by other user
-	let receivedFollow;
+	let receivedFollow: Follow | undefined;
 	try {
 		receivedFollow = await prisma.follow.findFirst({
 			where: { giverId: req.body.id, receiverId: req.user.id },
@@ -118,10 +133,14 @@ export const findFollow = async (req, res, next) => {
 };
 
 // Gets the follows given by (any) user
-export const getGivenFollows = async (req, res, next) => {
+export const getGivenFollows = async (
+	req: AuthReq & HasId & HasLimit,
+	res: Response,
+	next: NextFunction
+) => {
 	// First, confirm if provided user exists
 	// If no user is found, handle it at the top-level (server.ts) as 500 error
-	let otherUser;
+	let otherUser: User | undefined;
 	try {
 		otherUser = await prisma.user.findUnique({
 			where: { id: req.body.id },
@@ -138,7 +157,7 @@ export const getGivenFollows = async (req, res, next) => {
 	}
 
 	// Second, retrieve given follows
-	let givenFollows;
+	let givenFollows: Follow[] | undefined;
 	try {
 		givenFollows = await prisma.follow.findMany({
 			where: { giverId: req.body.id },
@@ -155,10 +174,14 @@ export const getGivenFollows = async (req, res, next) => {
 };
 
 // Gets the follows received by (any) user
-export const getReceivedFollows = async (req, res, next) => {
+export const getReceivedFollows = async (
+	req: AuthReq & HasId & HasLimit,
+	res: Response,
+	next: NextFunction
+) => {
 	// First, confirm if provided user exists
 	// If no user is found, handle it at the top-level (server.ts) as 500 error
-	let otherUser;
+	let otherUser: User | undefined;
 	try {
 		otherUser = await prisma.user.findUnique({
 			where: { id: req.body.id },
@@ -175,7 +198,7 @@ export const getReceivedFollows = async (req, res, next) => {
 	}
 
 	// Second, retrieve received follows
-	let receivedFollows;
+	let receivedFollows: Follow[];
 	try {
 		receivedFollows = await prisma.follow.findMany({
 			where: { receiverId: req.body.id },
