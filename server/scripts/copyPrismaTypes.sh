9ec3a6161fba6402eb160878f60952b237c3bf38
Mark Pieciak <58556319+mpieciak18@users.noreply.github.com>
2023-12-14 14:27:31 -0500

    update how types are shared from node_modules to shared folder

--- server/scripts/copyPrismaTypes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp -r ../server/node_modules/@prisma/client ../shared/types/@prisma/client
-cp -r ../server/node_modules/.prisma/client ../shared/types/.prisma/client
-echo "Prisma types copied!"
